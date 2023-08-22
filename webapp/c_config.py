e55881fab0668ad8e1884776024f9f4d7c1aed83
Andrey Pakhomenkov <pakhomenkov.ap@gmail.com>
2023-08-22 10:47:07 +0300

    что-то тут наваял, но в целом не нравится, надо подумать еще

--- webapp/c_config.py
+++ webapp/c_config.py
@@ -4,11 +4,13 @@ import os
 
 class Config():
 
-    APPLICATION_NAME = "PyMemorium"
-    PORT = 7777
-    PROGRAM_VERSION = "0.11"
+    APPLICATION_NAME: str = "PyMemorium"
+    PORT: int = 7777
+    PROGRAM_VERSION: str = "0.11"
     SECRET_KEY = os.urandom(24)
-    LOGS_PATH = "D:\\home\\projects\\pymemorium\\logs\\main.log"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///DB/pymemorium.sqlite'
+    LOGS_PATH: str = "D:\\home\\projects\\pymemorium\\logs\\main.log"
     # SOURCE_PATH = "/home/template/Dropbox/notebook"
-
+    ALCHEMY_ECHO: int = 1
+    DB_PATH: str = "D:\\home\\YandexDisk\\Private\\app_data\\PyMemorium\\"
+    DB_NAME: str = "pymemorium.db"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}{DB_NAME}"
